44a54094d45ce4715f2c41bcb058ec465d3f252b
larissamiyaji <61712566+larissamiyaji@users.noreply.github.com>
2020-03-26 12:26:01 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let name = 
+
+//let name = document.querySelector("input");
+
+
+//getElementById('name');
+//name.innerHTML="Olá, " + name + ". Seja bem-vindo(a) ao Pergunta e Respostas. Escolha um tema para jogar!"";
+
+
+
+
+
+//let name = getElementById('name');
+//name.innerHTML="Olá" + name + "! Agora escolha em qual categoria deseja jogar"
+
+//let saudation = `Olá, ${name}, seja bem-vindo(a) ao Pergunta e Respostas. Escolha um tema para jogar!`
